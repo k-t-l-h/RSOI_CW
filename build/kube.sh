8a081284cde21170d84620a7e37618e0694b0b22
k-t-l-h <laciedreamer@gmail.com>
2021-09-10 00:40:25 +0300

    add some files

--- build/kube.sh
+++ build/kube.sh
@@ -2,4 +2,7 @@
 
 kubectl create deployment auth --image=docker.io/ktlh/auth
 kubectl expose deployment auth --type=LoadBalancer --port=8080
-minikube service auth
+kubectl apply -f auth.yaml
+kubectl get deployments hello-node
+kubectl describe deployments hello-node
+minikube service hello-node
