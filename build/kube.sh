b89fb006e3f40cd2198fb8b4028c94af7a09eb3d
k-t-l-h <laciedreamer@gmail.com>
2021-09-11 11:01:34 +0300

    add kompose

--- build/kube.sh
+++ build/kube.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-kubectl create deployment auth --image=docker.io/ktlh/auth
-kubectl expose deployment auth --type=LoadBalancer --port=8080
-kubectl apply -f auth.yaml
-kubectl get deployments hello-node
-kubectl describe deployments hello-node
-kubectl apply -f ingress.yaml
-minikube service hello-node
+kompose convert
+#kubectl create deployment auth --image=docker.io/ktlh/auth
+#kubectl expose deployment auth --type=LoadBalancer --port=8080
+#kubectl apply -f auth.yaml
+#kubectl get deployments hello-node
+#kubectl describe deployments hello-node
+#kubectl apply -f ingress.yaml
+#minikube service hello-node
