13e3538775cddaab231fd2a1ffaae816b5ab0686
k-t-l-h <laciedreamer@gmail.com>
2021-09-16 14:44:17 +0300

    more tests

--- internal/pkg/airport/delivery/handler_test.go
+++ internal/pkg/airport/delivery/handler_test.go
@@ -1,23 +1,31 @@
 package delivery
 
 import (
+	"RSOI_CW/internal/models"
 	"RSOI_CW/internal/pkg/airport"
 	"fmt"
 	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
 
 func TestGetAirport(t *testing.T) {
+
 	ctl := gomock.NewController(t)
 	defer ctl.Finish()
+	id := uuid.New()
 
 	mock := airport.NewMockIRepo(ctl)
 	handler := NewAirportHandler(mock)
 
-	mock.EXPECT().SelectAirport(gomock.Any()).Return().Times(0)
-	r := httptest.NewRequest("POST", "/persons", strings.NewReader(fmt.Sprint()))
+	mock.EXPECT().SelectAirport(id).Return(models.Airport{}, models.StatusOkey).Times(1)
+	r := httptest.NewRequest("GET", "/airports/{UUID}", strings.NewReader(fmt.Sprint()))
+	r = mux.SetURLVars(r, map[string]string{
+		"UUID": id.String(),
+	})
 	w := httptest.NewRecorder()
 
 	handler.GetAirport(w, r)
@@ -30,8 +38,9 @@ func TestGetAirports(t *testing.T) {
 	mock := airport.NewMockIRepo(ctl)
 	handler := NewAirportHandler(mock)
 
-	mock.EXPECT().SelectAirports().Return().Times(0)
-	r := httptest.NewRequest("POST", "/persons", strings.NewReader(fmt.Sprint()))
+	mock.EXPECT().SelectAirports().Return([]models.Airport{}, models.StatusOkey).Times(1)
+	r := httptest.NewRequest("GET", "/airports", strings.NewReader(fmt.Sprint()))
+
 	w := httptest.NewRecorder()
 
 	handler.GetAirports(w, r)
---
+++ internal/pkg/bonus/delivery/http_test.go
@@ -0,0 +1,85 @@
+package delivery
+
+import (
+	"RSOI_CW/internal/models"
+	"RSOI_CW/internal/pkg/bonus"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestGetBonus(t *testing.T) {
+
+	os.Setenv("SECRET", "ABCD")
+	id := uuid.New()
+
+	tokenModel := models.Token{
+		UserUUID: id,
+		UserRole: "auth",
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Minute * 5).Unix(),
+		},
+	}
+
+	SecretKey := "ABCD"
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, tokenModel)
+
+	jwtCookie, _ := token.SignedString([]byte(SecretKey))
+
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	mock := bonus.NewMockIRepo(ctl)
+	handler := NewBonusHandler(mock)
+
+	mock.EXPECT().GetBonus(id).Return(0, models.StatusOkey).Times(1)
+	r := httptest.NewRequest("GET", "/airports/{UUID}", strings.NewReader(fmt.Sprint()))
+	r = mux.SetURLVars(r, map[string]string{
+		"UUID": id.String(),
+	})
+	r.Header.Set("Authorization", "Bearer "+jwtCookie)
+	w := httptest.NewRecorder()
+
+	handler.GetBonus(w, r)
+}
+
+func TestSetBonus(t *testing.T) {
+	os.Setenv("SECRET", "ABCD")
+	id := uuid.New()
+
+	tokenModel := models.Token{
+		UserUUID: id,
+		UserRole: "auth",
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Minute * 5).Unix(),
+		},
+	}
+
+	SecretKey := "ABCD"
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, tokenModel)
+
+	jwtCookie, _ := token.SignedString([]byte(SecretKey))
+
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	mock := bonus.NewMockIRepo(ctl)
+	handler := NewBonusHandler(mock)
+
+	mock.EXPECT().SetBonus(id, 1).Return(1, models.StatusOkey).Times(1)
+	r := httptest.NewRequest("GET", "/airports/{UUID}", strings.NewReader(fmt.Sprint()))
+	r = mux.SetURLVars(r, map[string]string{
+		"UUID": id.String(),
+	})
+	r.Header.Set("Authorization", "Bearer "+jwtCookie)
+	w := httptest.NewRecorder()
+
+	handler.AddBonus(w, r)
+}
